206da0cc8fcf73ef4b1671027f4adca5b7706213
krajniak <krajniak@speedbone.de>
2023-06-17 03:46:17 +0200

    Changed version in setup.py from 1.0.1 to 1.0 Changed version in __init__.py from 1.0 to 1.

--- netbox_sb_dc_accessdata/__init__.py
+++ netbox_sb_dc_accessdata/__init__.py
@@ -4,7 +4,7 @@ class SBDCAccessdataDataConfig(PluginConfig):
     name = 'netbox_sb_dc_accessdata'
     verbose_name = 'Datacenter Accessdata'
     description = 'SB DC Accessdata'
-    version = '1.0'
+    version = '1.1'
     base_url = 'accessdata'
 
 config = SBDCAccessdataDataConfig
